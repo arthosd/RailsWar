0ec6bdbe2ef37126893529cf200518e0e12a04ac
arthosd <elie.debs@outlook.fr>
2021-02-27 16:31:28 +0100

    Chagement de syntax

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-var express = require('express');
+import express from 'express';
+
 var app = express();
 
-app.set("view engine", 'ejs')
+app.set("view engine", 'ejs');
 
 app.get('/', function(req, res) {
     res.render('Templates/index')
