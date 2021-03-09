92e00018215fb3c6cd11bcb88c7c38dd8deacaed
Danil Berrah <danil.berrah@outlook.fr>
2021-03-09 15:21:58 +0100

    added navigation and a map in background in the accueil page

--- index.js
+++ index.js
@@ -6,8 +6,16 @@ app.set("view engine", 'ejs')
 
 app.use(express.static("Assets"));
 
-app.get('/', function(req, res) {
-    res.render('Templates/accueil')
-  });
+app.get('/', function (req, res) {
+  res.render('Templates/accueil')
+});
+
+app.get('/sign', function (req, res) {
+  res.render('Templates/sign')
+});
+
+app.get('/map', function(req, res) {
+  res.render('Templates/map')
+});
 
 app.listen(9080)
