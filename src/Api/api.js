3c56e90c4e9f17e23764ea78e5e68b9f1d5dc030
arthosd <elie.debs@outlook.fr>
2021-03-10 19:14:07 +0100

    Changement mineures

--- src/Api/api.js
+++ src/Api/api.js
@@ -1,1 +1,19 @@
-//
+// Toutes les fonctions pour récuperer les données de l'api
+import axios from 'axios';
+
+/**
+ * 
+ * @param { region on chercher les lingnes de metro } region 
+ * @returns Une promesse
+ */
+export function get_lines (region) {
+
+    const url = "https://api.sncf.com/v1/coverage/"+region+"/lines//?";
+
+    const auth = {
+        username :"20207a24-a66a-4324-8cdb-a9983d61bf65",
+        password : ''
+    }
+
+    return axios(url,auth);
+}
