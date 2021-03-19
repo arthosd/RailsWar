fc2c8cf695ce34ecfa4cf5bb8c47e8a181a2df8e
Thaly-fifi <thalyafossey1@hotmail.fr>
2021-03-19 17:38:18 +0100

    code cleaning en cours

--- index.js
+++ index.js
@@ -6,16 +6,14 @@ app.set("view engine", 'ejs')
 
 app.use(express.static("Assets"));
 
+app.use(express.static('/views'));
+
 app.get('/', function (req, res) {
   res.render('Templates/accueil')
 });
 
 app.get('/sign', function (req, res) {
   res.render('Templates/sign')
-});
-
-app.get('/map', function(req, res) {
-  res.render('Templates/map')
 });
 
 app.listen(9080)
